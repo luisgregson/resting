ac00bbcf74338b6e73e7c41e2d3377f7aac8ba71
mirkoperillo <mirko.perillo@gmail.com>
2019-08-06 06:48:49 +0200

    Import and export Resting format

--- src/js/app/bookmark.js
+++ src/js/app/bookmark.js
@@ -95,6 +95,205 @@ define(function() {
     return request;
   };
 
+  const exportObj = (bookmarks = [], contexts = []) => {
+      let harExport = {};
+      harExport.version = '1.1';
+      harExport.creator = {};
+      harExport.creator.name = 'Resting WebExtension';
+      harExport.creator.version = '1.0';
+      harExport.entries = _bookmarkToHar(bookmarks);
+      harExport._contexts = _contextsToHar(contexts);
+      return harExport;
+  };
+
+  const _contextsToHar = (contexts = []) => {
+
+      return contexts.map(c => {
+        let contextHarField = {};
+        contextHarField.name = c.name;
+        contextHarField.variables = c.variables.map(v => ({name: v.name, value: v.value, enabled: v.enabled}));
+        return contextHarField;
+        }
+      );
+  };
+
+  const _bookmarkToHar = (sources = []) => {
+    let exported = [];
+    if(sources.length > 0) {
+      let bookmarkExport = {};
+      bookmarkExport._name = sources[0].name;
+      bookmarkExport._isFolder = sources[0].isFolder;
+      bookmarkExport._id = sources[0].id;
+      bookmarkExport._created = sources[0].created;
+      bookmarkExport._folder = sources[0].folder;
+      bookmarkExport.headerSize = -1; // not supported
+      bookmarkExport.bodySize = -1; // not supported
+      if(sources[0].request) {
+        bookmarkExport.url = sources[0].request.url;
+        bookmarkExport.method = sources[0].request.method;
+        bookmarkExport.headers = sources[0].request.headers.map(h => ({name: h.name, value: h.value, _enabled: h.enabled}));
+        bookmarkExport.queryString = sources[0].request.querystring.map(q => ({name: q.name, value: q.value, _enabled: q.enabled}));
+        if(sources[0].request.body) {
+          bookmarkExport.postData = {};
+          bookmarkExport.postData.mimeType = _getMimeType(sources[0].request.bodyType);
+          if(bookmarkExport.postData.mimeType === 'application/x-www-form-urlencoded' || bookmarkExport.postData.mimeType === 'multipart/form-data') {
+            bookmarkExport.postData.params = sources[0].request.body.map(p => ({name: p.name, value: p.value, _enabled: p.enabled}));;
+          } else {
+            bookmarkExport.postData.text = sources[0].request.body;
+          }
+        }
+        bookmarkExport._authentication = sources[0].request.authentication;
+      }
+
+      exported.push(bookmarkExport);
+      if(bookmarkExport._isFolder) {
+        exported = exported.concat(_bookmarkToHar(sources[0].bookmarks));
+      }
+      exported = exported.concat(_bookmarkToHar(sources.slice(1)));
+    }
+
+    return exported;
+  };
+
+  const _getMimeType = repr => {
+    switch(repr){
+      case 'form-data':
+        return 'multipart/form-data';
+      case 'raw':
+        return 'application/json';
+      default:
+      case 'x-www-form-urlencoded':
+        return 'application/x-www-form-urlencoded';
+    }
+  };
+
+  const _getBodyType = mime => {
+    switch(mime){
+      case 'multipart/form-data':
+        return 'form-data';
+      case 'application/json':
+        return 'raw';
+      default:
+      case 'application/x-www-form-urlencoded':
+        return 'x-www-form-urlencoded';
+    }
+  };
+
+
+  const importObj = (obj = {}) => {
+      let importObj = {};
+      if(obj.entries) {
+        const indexRelationship = _extractRelationship(obj.entries);
+        importObj.bookmarks = obj.entries.map(e => _importEntry(e));
+        importObj.bookmarks = _fixStructure(importObj.bookmarks, indexRelationship);
+      }
+      if(obj._contexts) {
+        importObj.contexts = obj._contexts.map(e => _importContext(e));
+      }
+      return importObj;
+  };
+
+  const _fixStructure = (bookmarks, mapping) => {
+    let indexBookmarks = {};
+    let i = 0;
+    let bookmark;
+    for(bookmark of bookmarks) {
+      const bookmarkId = bookmark.id;
+      indexBookmarks[bookmarkId] = {obj: bookmark, position: i};
+      i++;
+    }
+    let folderId;
+    for( folderId of Object.keys(mapping) ) {
+      const insideBookmarkIds = mapping[folderId];
+      insideBookmarkIds.forEach(id => {
+        if(!indexBookmarks[folderId].obj.bookmarks) {
+          indexBookmarks[folderId].obj.bookmarks = [];
+        }
+        indexBookmarks[folderId].obj.bookmarks.push(indexBookmarks[id].obj);
+        bookmarks.splice(indexBookmarks[id].position,1);
+      });
+    }
+
+    return bookmarks;
+  }
+
+  const _extractRelationship = (entries = []) => {
+    let relationMapping = {};
+    entries.forEach((e) => {
+      const id = e._id;
+      const folderId = e._folder;
+      if(folderId) {
+        if(!relationMapping.folderId) {
+          relationMapping[folderId] = [];
+        }
+        relationMapping[folderId].push(id);
+      }
+    });
+
+    return relationMapping;
+  };
+
+  // importEntry dovrebbe ritornare una tupla, lista flat
+  // bookmarks e mappa relazini
+  // ATTENZIONE: importEntry lavora su singola entry
+  // SCRITTO FUNZIONE
+
+  const _importEntry = (entry = {}) => {
+    let bookmark = {};
+    bookmark.request = {};
+    if(entry._isFolder) {
+      bookmark.isFolder = entry._isFolder;
+    }
+    if(entry._name) {
+      bookmark.name = entry._name;
+    }
+    if(entry._id) {
+      bookmark.id = entry._id;
+    }
+    if(entry._created) {
+      bookmark.created = entry._created;
+    }
+    if(entry.url) {
+      bookmark.request.url = entry.url;
+    }
+    if(entry.method) {
+      bookmark.request.method = entry.method;
+    }
+    if(entry.headers) {
+      bookmark.request.headers = entry.headers.map(h => ({name: h.name, value: h.value, enabled: h._enabled}));
+    }
+
+    if(entry.queryString) {
+      bookmark.request.querystring = entry.queryString.map(h => ({name: h.name, value: h.value, enabled: h._enabled}));
+    }
+    if(entry._authentication) {
+      bookmark.request.authentication = entry._authentication;
+    }
+    if(entry.postData) {
+      bookmark.request.bodyType = _getBodyType(entry.postData.mimeType);
+      if(entry.postData.mimeType === 'multipart/form-data' || entry.postData.mimeType === 'application/x-www-form-urlencoded') {
+        bookmark.request.body = JSON.stringify(entry.postData.params.map(p => ({name: p.name, value: p.value, enabled: p._enabled})));
+      } else {
+        bookmark.request.body = entry.postData.text;
+      }
+    }
+    return bookmark;
+  };
+
+  const _importContext = (entry = {}) => {
+    let context = {};
+    if(entry.name) {
+      context.name = entry.name;
+    }
+    if(entry.variables) {
+      context.variables = entry.variables.map(v => ({name: v.name, value: v.value, enabled: v.enabled}));
+    }
+
+    return context;
+  };
+
+
+
   return function(storageProvider) {
       return {
         makeBookmark : makeBookmark,
@@ -106,6 +305,8 @@ define(function() {
         replaceBookmark : replaceBookmark,
         save : bookmark => storageProvider.save(bookmark),
         importHAR : importHAR(storageProvider),
+        exportObj,
+        importObj,
       };
   };
 });
--- test/bookmarkSpec.js
+++ test/bookmarkSpec.js
@@ -1121,8 +1121,201 @@ describe("Import HAR", function() {
   });
 });
 
+describe("Export", function() {
+  it("no bookmarks", function() {
+    const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    const response = bookmarkProvider.exportObj([]);
+    expect('1.1').toBe(response.version);
+    expect('Resting WebExtension').toBe(response.creator.name);
+    expect('1.0').toBe(response.creator.version);
+  });
 
 
+   it("folder with one bookmark", function() {
+    const bookmarks = '[{"id":"7e1a4356-fede-41c3-b764-5024c8f17188","name":"FOLDER","bookmarks":[{"id":"618c48e8-7336-474a-8263-525da96c0cc5","name":"my-bookmark","isFolder":false,"folder":"7e1a4356-fede-41c3-b764-5024c8f17188","requestMethod":"PUT","requestUrl":"www.google.it","request":{"method":"PUT","url":"www.google.it","headers":[{"name":"header1","value":"v1","enabled":true},{"name":"header2","value":"v2","enabled":true}],"querystring":[],"bodyType":"raw","body":"{\\n\\"field\\" : \\"value\\" \\n}","authentication":{"type":"Basic","username":"gino","password":"pilo"},"context":"default"},"created":"2019-05-28T04:36:48.322Z"}],"isFolder":true,"created":"2019-05-28T04:36:38.346Z"}]';
+    const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    const response = bookmarkProvider.exportObj(JSON.parse(bookmarks));
+    expect('1.1').toBe(response.version);
+    expect('Resting WebExtension').toBe(response.creator.name);
+    expect('1.0').toBe(response.creator.version);
+    expect(2).toBe(response.entries.length);
+    expect("FOLDER").toBe(response.entries[0]._name);
+    expect(true).toBe(response.entries[0]._isFolder);
+    expect("7e1a4356-fede-41c3-b764-5024c8f17188").toBe(response.entries[0]._id);
+    expect("2019-05-28T04:36:38.346Z").toBe(response.entries[0]._created);
+    expect('my-bookmark').toBe(response.entries[1]._name);
+    expect('www.google.it').toBe(response.entries[1].url)
+    expect('PUT').toBe(response.entries[1].method);
+    expect(2).toBe(response.entries[1].headers.length);
+    expect(true).toBe(response.entries[1].headers[1]._enabled);
+    expect("application/json").toBe(response.entries[1].postData.mimeType);
+    expect('{\n\"field\" : \"value\" \n}').toBe(response.entries[1].postData.text);
+    expect('gino').toBe(response.entries[1]._authentication.username);
+  });
 
+  it("bookmark post request urlencoded", function() {
+    const bookmarks = '[{"id":"9406075c-e754-4d9b-a264-cb6b9f4fd9ea","request":{"method":"POST","url":"test.local","headers":[],"querystring":[],"bodyType":"x-www-form-urlencoded","body":[{"name":"ccc","value":"ooo","enabled":true},{"name":"qqq","value":"ppp","enabled":false}],"authentication":{"username":"","password":""},"context":"default"},"name":"book","isFolder":false,"created":"2019-06-11T04:49:40.308Z"}]';
+    const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    const response = bookmarkProvider.exportObj(JSON.parse(bookmarks));
+    expect('1.1').toBe(response.version);
+    expect('Resting WebExtension').toBe(response.creator.name);
+    expect('1.0').toBe(response.creator.version);
+    expect(1).toBe(response.entries.length);
+    expect("book").toBe(response.entries[0]._name);
+    expect(false).toBe(response.entries[0]._isFolder);
+    expect("9406075c-e754-4d9b-a264-cb6b9f4fd9ea").toBe(response.entries[0]._id);
+    expect("2019-06-11T04:49:40.308Z").toBe(response.entries[0]._created);
+    expect('test.local').toBe(response.entries[0].url)
+    expect('POST').toBe(response.entries[0].method);
+    expect(0).toBe(response.entries[0].headers.length);
+    expect("application/x-www-form-urlencoded").toBe(response.entries[0].postData.mimeType);
+    expect(response.entries[0].postData.text).toBeUndefined();
+    expect(response.entries[0].postData.params.length).toBe(2);
+    expect('').toBe(response.entries[0]._authentication.username);
+  });
+
+  it("bookmark post request form-data", function() {
+    const bookmarks = '[{"id":"2cf18425-2f1c-4491-9706-930f6c679f9b","request":{"method":"POST","url":"google.com","headers":[],"querystring":[],"bodyType":"form-data","body":[{"name":"p1","value":"v1","enabled":true},{"name":"p2","value":"v2","enabled":true},{"name":"p3","value":"v3","enabled":true}],"authentication":{"username":"","password":""},"context":"default"},"name":"test","isFolder":false,"created":"2019-06-14T04:29:43.764Z"}]';
+    const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    const response = bookmarkProvider.exportObj(JSON.parse(bookmarks));
+    expect(response.version).toBe('1.1');
+    expect(response.creator.name).toBe('Resting WebExtension');
+    expect(response.creator.version).toBe('1.0');
+    expect(response.entries.length).toBe(1);
+    expect(response.entries[0]._name).toBe("test");
+    expect(response.entries[0]._isFolder).toBe(false);
+    expect(response.entries[0]._id).toBe("2cf18425-2f1c-4491-9706-930f6c679f9b");
+    expect(response.entries[0]._created).toBe("2019-06-14T04:29:43.764Z");
+    expect(response.entries[0].url).toBe('google.com')
+    expect(response.entries[0].method).toBe('POST');
+    expect(response.entries[0].headers.length).toBe(0);
+    expect(response.entries[0].postData.mimeType).toBe("multipart/form-data");
+    expect(response.entries[0].postData.text).toBeUndefined();
+    expect(response.entries[0].postData.params.length).toBe(3);
+    expect(response.entries[0]._authentication.username).toBe('');
+  });
+
+  it("contexts", function() {
+    const contexts = '[{"name":"default","variables":[{"name":"def","value":"1111","enabled":true}]},{"name":"dev","variables":[{"name":"p1","value":"v1","enabled":true},{"name":"p2","value":"v2","enabled":true},{"name":"p3","value":"v3","enabled":true}]},{"name":"prod","variables":[{"name":"var1","value":"value","enabled":true},{"name":"var2","value":"value-2","enabled":true}]}]';
+    const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    const response = bookmarkProvider.exportObj([],JSON.parse(contexts));
+    expect(response.version).toBe('1.1');
+    expect(response.creator.name).toBe('Resting WebExtension');
+    expect(response.creator.version).toBe('1.0');
+    expect(response.entries.length).toBe(0);
+    expect(response._contexts.length).toBe(3);
+    expect(response._contexts[0].name).toBe('default');
+    expect(response._contexts[0].variables.length).toBe(1);
+    expect(response._contexts[0].variables[0]).toEqual({name:'def', value:'1111', enabled:true});
+    expect(response._contexts[1].name).toBe('dev');
+    expect(response._contexts[1].variables.length).toBe(3);
+    expect(response._contexts[2].name).toBe('prod');
+    expect(response._contexts[2].variables.length).toBe(2);
+
+  });
+
+});
+
+
+describe("Import", function() {
+  it("no bookmarks", function() {
+    const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    const response = bookmarkProvider.importObj({});
+    expect(response.bookmarks).toBeUndefined();
+    expect(response.contexts).toBeUndefined();
+  });
+
+  it("bookmark of request multipart body", function() {
+    const sampleExport = `{"version":"1.1","creator":{"name":"Resting WebExtension","version":"1.0"},"entries":[{"_name":"test","_isFolder":false,"_id":"2cf18425-2f1c-4491-9706-930f6c679f9b","_created":"2019-06-14T04:29:43.764Z","headerSize":-1,"bodySize":-1,"url":"google.com","method":"POST","headers":[],"queryString":[],"postData":{"mimeType":"multipart/form-data","params":[{"name":"p1","value":"v1","_enabled":true},{"name":"p2","value":"v2","_enabled":true},{"name":"p3","value":"v3","_enabled":true}]},"_authentication":{"username":"","password":""}}],"_contexts":[]}`;
+    const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    const response = bookmarkProvider.importObj(JSON.parse(sampleExport));
+    expect(response.bookmarks.length).toBe(1);
+    expect(response.bookmarks[0].id).toBe('2cf18425-2f1c-4491-9706-930f6c679f9b');
+    expect(response.bookmarks[0].created).toBe('2019-06-14T04:29:43.764Z');
+    expect(response.bookmarks[0].name).toBe('test');
+    expect(response.bookmarks[0].request.url).toBe('google.com');
+    expect(response.bookmarks[0].request.method).toBe('POST');
+    expect(response.bookmarks[0].request.headers.length).toBe(0);
+    expect(response.bookmarks[0].request.querystring.length).toBe(0);
+    expect(response.bookmarks[0].request.authentication).toEqual({username:'',password:''});
+    expect(response.bookmarks[0].request.body).toBe('[{"name":"p1","value":"v1","enabled":true},{"name":"p2","value":"v2","enabled":true},{"name":"p3","value":"v3","enabled":true}]');
+    expect(response.bookmarks[0].request.bodyType).toBe('form-data');
+    expect(response.contexts.length).toBe(0);
+  });
+
+   it("bookmark of request JSON body", function() {
+    //const d = `[{"id":"f8432b7b-55de-4108-bb4c-e546580ad016","request":{"method":"POST","url":"test.url","headers":[{"name":"Contenxt","value":"application/json","enabled":true}],"querystring":[],"bodyType":"raw","body":"{\\n\\"field\\": \\"my-value\\",\\n\\"array\\": [1,2,3,4,5]\\n}","authentication":{"username":"","password":""},"context":"default"},"name":"json body request","isFolder":false,"created":"2019-06-20T04:48:34.974Z"}]`;
+    //const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    //const response = bookmarkProvider.exportObj(JSON.parse(d));
+    //console.log(JSON.stringify(response));
+    const sampleExport = `{"version":"1.1","creator":{"name":"Resting WebExtension","version":"1.0"},"entries":[{"_name":"json body request","_isFolder":false,"_id":"f8432b7b-55de-4108-bb4c-e546580ad016","_created":"2019-06-20T04:48:34.974Z","headerSize":-1,"bodySize":-1,"url":"test.url","method":"POST","headers":[{"name":"Contenxt","value":"application/json","_enabled":true}],"queryString":[],"postData":{"mimeType":"application/json","text":"{\\n\\"field\\": \\"my-value\\",\\n\\"array\\": [1,2,3,4,5]\\n}"},"_authentication":{"username":"","password":""}}],"_contexts":[]}`;
+    const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    const response = bookmarkProvider.importObj(JSON.parse(sampleExport));
+    expect(response.bookmarks.length).toBe(1);
+    expect(response.bookmarks[0].id).toBe('f8432b7b-55de-4108-bb4c-e546580ad016');
+    expect(response.bookmarks[0].created).toBe('2019-06-20T04:48:34.974Z');
+    expect(response.bookmarks[0].name).toBe('json body request');
+    expect(response.bookmarks[0].request.url).toBe('test.url');
+    expect(response.bookmarks[0].request.method).toBe('POST');
+    expect(response.bookmarks[0].request.headers.length).toBe(1);
+    expect(response.bookmarks[0].request.headers[0]).toEqual({name: 'Contenxt', value: 'application/json', enabled: true});
+    expect(response.bookmarks[0].request.querystring.length).toBe(0);
+    expect(response.bookmarks[0].request.authentication).toEqual({username:'',password:''});
+    expect(response.bookmarks[0].request.body).toBe('{\n\"field\": \"my-value\",\n\"array\": [1,2,3,4,5]\n}');
+    expect(response.bookmarks[0].request.bodyType).toBe('raw');
+    expect(response.contexts.length).toBe(0);
+  });
+
+
+   it("folder", function() {
+    //const d = `[{"id":"1078d8a7-bfa9-45d2-b8ef-47a808ebfb88","name":"My folder","bookmarks":[],"isFolder":true,"created":"2019-06-21T04:38:42.163Z"}]`;
+    //const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    //const response = bookmarkProvider.exportObj(JSON.parse(d));
+    //console.log(JSON.stringify(response));
+
+    const sampleExport = `{"version":"1.1","creator":{"name":"Resting WebExtension","version":"1.0"},"entries":[{"_name":"My folder","_isFolder":true,"_id":"1078d8a7-bfa9-45d2-b8ef-47a808ebfb88","_created":"2019-06-21T04:38:42.163Z","headerSize":-1,"bodySize":-1}],"_contexts":[]}`;
+    const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    const response = bookmarkProvider.importObj(JSON.parse(sampleExport));
+    expect(response.bookmarks.length).toBe(1);
+    expect(response.bookmarks[0].isFolder).toBe(true);
+    expect(response.bookmarks[0].id).toBe('1078d8a7-bfa9-45d2-b8ef-47a808ebfb88');
+    expect(response.bookmarks[0].created).toBe('2019-06-21T04:38:42.163Z');
+    expect(response.bookmarks[0].name).toBe('My folder');
+    expect(response.contexts.length).toBe(0);
+  });
+
+
+   it("contexts", function() {
+    //const d = `[{"name":"default","variables":[{"name":"nano","value":"nano","enabled":true},{"name":"paco","value":"paco","enabled":true}]}, {"name":"dev","variables":[{"name":"p1","value":"v1","enabled":false}]}]`;
+    //const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    //const response = bookmarkProvider.exportObj([],JSON.parse(d));
+    //console.log(JSON.stringify(response));
+
+    const sampleExport = `{"version":"1.1","creator":{"name":"Resting WebExtension","version":"1.0"},"entries":[],"_contexts":[{"name":"default","variables":[{"name":"nano","value":"nano","enabled":true},{"name":"paco","value":"paco","enabled":true}]},{"name":"dev","variables":[{"name":"p1","value":"v1","enabled":false}]}]}`;
+    const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    const response = bookmarkProvider.importObj(JSON.parse(sampleExport));
+    expect(response.bookmarks.length).toBe(0);
+    expect(response.contexts.length).toBe(2);
+    expect(response.contexts[1].name).toBe('dev');
+    expect(response.contexts[1].variables[0]).toEqual({name: 'p1', value: 'v1', enabled: false});
+    expect(response.contexts[0].name).toBe('default');
+    expect(response.contexts[0].variables.length).toBe(2);
+  });
+
+  it("a bookmark inside folder, a bookmark outside", function() {
+    //const d = `[{"id":"5fff2aa9-6347-480c-8520-a0e3d586d667","request":{"method":"GET","url":"google.com","headers":[],"querystring":[],"authentication":{"username":"","password":""},"context":"default"},"isFolder":false,"created":"2019-07-27T18:51:37.270Z"}, {"id":"cdd58512-418d-4254-85d5-134e298886fd","name":"FOLDER","bookmarks":[{"id":"b0d3f619-c73d-42aa-87b1-a7c44dc01227","name":"book","isFolder":false,"folder":"cdd58512-418d-4254-85d5-134e298886fd","requestMethod":"GET","requestUrl":"example.com","request":{"method":"GET","url":"example.com","headers":[],"querystring":[],"authentication":{"username":"","password":""},"context":"default"},"created":"2019-07-27T18:51:28.894Z"}],"isFolder":true,"created":"2019-07-27T18:51:13.483Z"}]`;
+    //const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    //const response = bookmarkProvider.exportObj(JSON.parse(d));
+    //console.log(JSON.stringify(response));
+
+    const sampleExport = `{"version":"1.1","creator":{"name":"Resting WebExtension","version":"1.0"},"entries":[{"_isFolder":false,"_id":"5fff2aa9-6347-480c-8520-a0e3d586d667","_created":"2019-07-27T18:51:37.270Z","headerSize":-1,"bodySize":-1,"url":"google.com","method":"GET","headers":[],"queryString":[],"_authentication":{"username":"","password":""}},{"_name":"FOLDER","_isFolder":true,"_id":"cdd58512-418d-4254-85d5-134e298886fd","_created":"2019-07-27T18:51:13.483Z","headerSize":-1,"bodySize":-1},{"_name":"book","_isFolder":false,"_id":"b0d3f619-c73d-42aa-87b1-a7c44dc01227","_created":"2019-07-27T18:51:28.894Z","_folder":"cdd58512-418d-4254-85d5-134e298886fd","headerSize":-1,"bodySize":-1,"url":"example.com","method":"GET","headers":[],"queryString":[],"_authentication":{"username":"","password":""}}],"_contexts":[]}`;
+    const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+    const response = bookmarkProvider.importObj(JSON.parse(sampleExport));
+    expect(response.bookmarks.length).toBe(2);
+    expect(response.contexts.length).toBe(0);
+    expect(response.bookmarks[1].name).toBe('FOLDER');
+    expect(response.bookmarks[1].bookmarks.length).toBe(1);
+  });
+});
 
 
